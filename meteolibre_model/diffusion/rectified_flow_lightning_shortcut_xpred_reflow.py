00608f6b882ed0be946a9882614ec64ca66a9c52
Adrien B <adrienbufort@gmail.com>
2026-03-27 10:27:06 +0100

    feat: update model noise for improve rollout training

--- meteolibre_model/diffusion/rectified_flow_lightning_shortcut_xpred_reflow.py
+++ meteolibre_model/diffusion/rectified_flow_lightning_shortcut_xpred_reflow.py
@@ -167,8 +167,8 @@ def trainer_step(
     t_emp = torch.rand(num_emp, device=device)
 
     # log norm sampling for t
-    eps = torch.randn(num_emp, device=device)
-    t_emp = torch.sigmoid(0.4 + 1.2 * eps).clamp(1e-4, 1 - 1e-4)
+    # eps = torch.randn(num_emp, device=device)
+    # t_emp = torch.sigmoid(0.4 + 1.2 * eps).clamp(1e-4, 1 - 1e-4)
 
     # progressive noise
     # if sigma > 0:
