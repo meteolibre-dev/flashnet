8a350327a17c52d1b32138d72a5158ea0dde19a2
Adrien B <forbu14@gmail.com>
2026-07-22 21:38:20 +0200

    feat: update model

--- meteolibre_model/diffusion/rectified_flow_lightning_shortcut_xpred_blur_v2.py
+++ meteolibre_model/diffusion/rectified_flow_lightning_shortcut_xpred_blur_v2.py
@@ -294,7 +294,7 @@ def apply_blur_with_sigma_batched(x, blur_sigma, n_bins=8, min_kernel=0, sigma_f
 
 def trainer_step(
     model, batch, device, sigma=0.0, parametrization="standard", interpolation="linear", use_residual=True,
-    grad_weight=0.0,
+    grad_weight=0.1,
     poly_power=10.0,
     noise_rho=0.,
     temporal_weight_scale=1.0,
@@ -303,7 +303,7 @@ def trainer_step(
     context_spec_noise_frame_ramp=0.0,
     d_x0_blur_prob=0.,
     d_x0_blur_scale=5.0,
-    temporal_grad_weight=0.0,
+    temporal_grad_weight=0.1,
 ):
     if parametrization != "standard":
         raise ValueError("Only 'standard' parametrization is supported for x-prediction.")
